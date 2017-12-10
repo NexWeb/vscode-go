2c77a1ccc5ef7fa5dd5572ac8890c0fae6d2d311
Nexus Web Development <github@nexwebsites.com>
2017-12-10 14:07:19 -0500

    Update terminateProcess.sh

--- scripts/terminateProcess.sh
+++ scripts/terminateProcess.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-terminateTree() {
-	for cpid in $(/usr/bin/pgrep -P $1); do
-		terminateTree $cpid
-		kill -9 $1 > /dev/null 2>&1
-done
-}
-
-for pid in $*; do
-	terminateTree $pid
-done
+pkill debug
